0a513f512825ddc0d5bed7582ad267fe09b8bcac
andimarek <andimarek@fastmail.fm>
2017-08-05 03:01:45 +0200

    add async serial execution strategy

--- src/test/groovy/graphql/execution/AsyncSerialExecutionStrategyTest.groovy
+++ src/test/groovy/graphql/execution/AsyncSerialExecutionStrategyTest.groovy
@@ -17,8 +17,7 @@ import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition
 import static graphql.schema.GraphQLObjectType.newObject
 import static graphql.schema.GraphQLSchema.newSchema
 
-class SerialExecutionStrategyTest extends Specification {
-
+class AsyncSerialExecutionStrategyTest extends Specification {
     GraphQLSchema schema(DataFetcher dataFetcher1, DataFetcher dataFetcher2, DataFetcher dataFetcher3) {
         GraphQLFieldDefinition.Builder fieldDefinition = newFieldDefinition()
                 .name("hello")
@@ -89,12 +88,84 @@ class SerialExecutionStrategyTest extends Specification {
                 .fields(['hello': [new Field('hello')], 'hello2': [new Field('hello2')], 'hello3': [new Field('hello3')]])
                 .build()
 
-        SerialExecutionStrategy strategy = new SerialExecutionStrategy()
+        AsyncSerialExecutionStrategy strategy = new AsyncSerialExecutionStrategy()
         when:
         def result = strategy.execute(executionContext, executionStrategyParameters)
 
 
         then:
+        result.isDone()
+        result.get().data == ['hello': 'world1', 'hello2': 'world2', 'hello3': 'world3']
+    }
+
+    def "async serial execution test"() {
+        given:
+        def df1 = Mock(DataFetcher)
+        def cf1 = new CompletableFuture()
+
+        def df2 = Mock(DataFetcher)
+        def cf2 = new CompletableFuture()
+
+        def df3 = Mock(DataFetcher)
+        def cf3 = new CompletableFuture()
+
+        GraphQLSchema schema = schema(df1, df2, df3)
+        String query = "{hello, hello2, hello3}"
+        def document = new Parser().parseDocument(query)
+
+        def typeInfo = ExecutionTypeInfo.newTypeInfo()
+                .type(schema.getQueryType())
+                .build()
+
+        ExecutionContext executionContext = new ExecutionContextBuilder()
+                .graphQLSchema(schema)
+                .executionId(ExecutionId.generate())
+                .document(document)
+                .valuesResolver(new ValuesResolver())
+                .instrumentation(NoOpInstrumentation.INSTANCE)
+                .build()
+        ExecutionStrategyParameters executionStrategyParameters = ExecutionStrategyParameters
+                .newParameters()
+                .typeInfo(typeInfo)
+                .fields(['hello': [new Field('hello')], 'hello2': [new Field('hello2')], 'hello3': [new Field('hello3')]])
+                .build()
+
+        AsyncSerialExecutionStrategy strategy = new AsyncSerialExecutionStrategy()
+        when:
+        def result = strategy.execute(executionContext, executionStrategyParameters)
+
+
+        then:
+        !result.isDone()
+        1 * df1.get(_) >> cf1
+        0 * df2.get(_) >> cf2
+        0 * df3.get(_) >> cf3
+
+        when:
+        cf1.complete("world1")
+
+        then:
+        !result.isDone()
+        0 * df1.get(_) >> cf1
+        1 * df2.get(_) >> cf2
+        0 * df3.get(_) >> cf3
+
+        when:
+        cf2.complete("world2")
+
+        then:
+        !result.isDone()
+        0 * df1.get(_) >> cf1
+        0 * df2.get(_) >> cf2
+        1 * df3.get(_) >> cf3
+
+        when:
+        cf3.complete("world3")
+
+        then:
+        0 * df1.get(_) >> cf1
+        0 * df2.get(_) >> cf2
+        0 * df3.get(_) >> cf3
         result.isDone()
         result.get().data == ['hello': 'world1', 'hello2': 'world2', 'hello3': 'world3']
     }
